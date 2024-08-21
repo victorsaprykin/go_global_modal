7fd8312bfc435fd68f7f03a40da8d44b33ec7e9c
Victor Saprykin <victanik@mail.ru>
2024-08-21 12:48:09 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const modalBtn = $('.header__btn');
+const modalClose = $('.close__btn');
+const overlay = $('.overlay');
+const modal = $('.modal');
+const body = $('body');
+
+modalBtn.click(function() {
+  overlay.show(500);
+  modal.css({opacity: '1'});
+  body.addClass('noscroll');
+});
+
+modalClose.click(function() {
+  overlay.hide(500);
+  modal.css({ opacity: '0' });
+  body.removeClass('.noscroll');
+});
+
+overlay.click(function() {
+  overlay.hide(500);
+  modal.css({ opacity: '0' });
+  body.removeClass('.noscroll');
+});
